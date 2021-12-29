196c1b1759d209c9fec5b3a01ab62ff27ab2f237
Ragu Subramaniyam <raguct25@gmail.com>
2021-12-29 16:21:17 +0530

    close event

--- src/app/sseEventService.ts
+++ src/app/sseEventService.ts
@@ -3,29 +3,29 @@ import { Observable } from 'rxjs';
 import { SseService } from './sse.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SseEventService {
-
   constructor(private zone: NgZone, private sseService: SseService) {}
 
   getServerSentEvent(url: string) {
     console.log('url', url);
-    
+
     return new Observable((observer: any) => {
       console.log('observer called', observer);
-      
+
       const eventSource = this.sseService.getEventSource(url);
 
       eventSource.onmessage = (event: any) => {
         this.zone.run(() => {
           console.log('onmessage called', event);
-          
+
           observer.next(event);
         });
       };
 
       eventSource.onerror = (error: any) => {
+        eventSource.close();
         this.zone.run(() => {
           observer.error('Error calling', error);
         });
